dab7aa6c8bb7b9b50087ae1dc7f05489b3d8452e
Bobomurod2004 <bobomurodgoibnazarov@gmail.com>
2026-05-21 03:35:52 +0500

    media fayl yo'lida xatolik to''grilandi

--- core/settings.py
+++ core/settings.py
@@ -230,7 +230,7 @@ STORAGES = {
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = config('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
